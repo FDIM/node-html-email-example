aaf2cdcc2290ef56e5da3ab752e25a9362917eae
Domas Trijonis <domas.trijonis@maersk.com>
2016-06-23 10:54:52 +0200

    email example with embedded images

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
 var juice = require('juice');
 var angularTemplate = require('angular-template');
 var nodemailer = require('nodemailer');
+var fs = require('fs');
 
-var smtpServerURL = 'smtp://<username>:<password>@<host>:<port>';
+var basePath = './emails/';
 var data = {
   name:'Tester',
   unsubscribeLink:'http://www.google.com'
 };
 
 var tpl = angularTemplate('emails/template.html', data);
-var inlined = juice(tpl);
 
-var transporter = nodemailer.createTransport(smtpServerURL);
+var transporter = nodemailer.createTransport({
+  service:'Gmail',
+  auth: {
+    user:'my@gmail.com',
+    pass: "<key>"
+  },
+  debug:true,
+  logger:true,
+  proxy: process.env.HTTP_PROXY
+});
 // setup e-mail data with unicode symbols
 var mailOptions = {
-    from: 'no-reply@example.com', // sender address
-    to: 'my@example.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world 🐴', // plaintext body
-    html: inlined // html body
+    from: 'my@gmail.com', // sender address
+    to: process.argv[2], // list of receivers
+    subject: "Hi "+(new Date()).getTime(), // Subject line
+    text: "Hi "+(new Date()).getTime(), // plaintext body
+    html: '', // html body
+    attachments:[]
 };
+var images = [];
+tpl.replace(/inline-image="([a-zA-Z0-9\-\/\\ \.]+)"/g, function(match, path){
+  images.push(path);
+});
+var prefix = (new Date()).getTime()+'';
+images.forEach((path)=>{
+  var cid = prefix+path+'@gmail.com';
+  tpl = tpl.replace('inline-image="'+path+'"','src="cid:'+cid+'"');
+  mailOptions.attachments.push({
+    filename:path,
+    cid:cid,
+    content: fs.createReadStream(basePath+path),
+    contentType:'image/jpeg',
+    contentDisposition:'inline'
+  });
+});
 
+mailOptions.html = juice(tpl);
+console.info(mailOptions.html);
 // send mail with defined transport object
 transporter.sendMail(mailOptions, function(error, info){
     if(error){
